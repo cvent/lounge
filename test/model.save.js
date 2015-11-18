dd53f25a4aa4b7f48f56fa90c700db58a4933bb2
Bojan Djurkovic <dbojan@gmail.com>
2015-11-18 12:13:52 -0400

    more model.save() tests

--- test/model.save.js
+++ test/model.save.js
@@ -1,10 +1,12 @@
+var couchbase = require('couchbase');
 var expect = require('chai').expect;
+
 var lounge = require('../lib');
 var Schema = lounge.Schema;
-var couchbase = require('couchbase');
-var cluster, bucket;
 
-describe('Model save tests', function () {
+var bucket;
+
+describe.only('Model save tests', function () {
   beforeEach(function (done) {
     lounge = new lounge.Lounge(); // recreate it
 
@@ -16,7 +18,7 @@ describe('Model save tests', function () {
     });
   });
 
-  it.only('should save a simple document', function (done) {
+  it('should save a simple document', function (done) {
     var userSchema = lounge.schema({
       firstName: String,
       lastName: String,
@@ -61,6 +63,124 @@ describe('Model save tests', function () {
           firstName: 'Joe',
           lastName: 'Smith',
           email: 'joe@gmail.com',
+          dateOfBirth: dob.toISOString()
+        };
+
+        expected.id = savedDoc.getDocumentKeyValue(true);
+
+        expect(dbDoc.value).to.deep.equal(expected);
+        done();
+      });
+    });
+  });
+
+  it('should save a simple document with data passed in to save()', function (done) {
+    var userSchema = lounge.schema({
+      firstName: String,
+      lastName: String,
+      email: String,
+      dateOfBirth: Date
+    });
+
+    var User = lounge.model('User', userSchema);
+
+    var dob = new Date('March 3, 1989 03:30:00');
+
+    var user = new User();
+
+    var data = {
+      firstName: 'Joe2',
+      lastName: 'Smith2',
+      email: 'joe2@gmail.com',
+      dateOfBirth: dob
+    };
+
+    user.save(data, function (err, savedDoc) {
+      expect(err).to.not.be.ok;
+
+      expect(savedDoc).to.be.ok;
+      expect(savedDoc).to.be.an('object');
+      expect(savedDoc.id).to.be.ok;
+      expect(savedDoc.id).to.be.a('string');
+
+      expect(savedDoc.firstName).to.be.equal('Joe2');
+      expect(savedDoc.lastName).to.be.equal('Smith2');
+      expect(savedDoc.email).to.be.equal('joe2@gmail.com');
+      expect(savedDoc.dateOfBirth).to.be.ok;
+      expect(savedDoc.dateOfBirth).to.be.an.instanceof(Date);
+      expect(savedDoc.dateOfBirth.toString()).to.be.equal((new Date(1989, 2, 3, 3, 30, 0)).toString());
+
+      bucket.get(savedDoc.getDocumentKeyValue(true), function (err, dbDoc) {
+        expect(err).to.not.be.ok;
+
+        expect(dbDoc).to.be.ok;
+        expect(dbDoc.value).to.be.ok;
+        expect(dbDoc.value).to.be.an('object');
+
+        var expected = {
+          firstName: 'Joe2',
+          lastName: 'Smith2',
+          email: 'joe2@gmail.com',
+          dateOfBirth: dob.toISOString()
+        };
+
+        expected.id = savedDoc.getDocumentKeyValue(true);
+
+        expect(dbDoc.value).to.deep.equal(expected);
+        done();
+      });
+    });
+  });
+
+  it('should save a simple document with some data passed in to save()', function (done) {
+    var userSchema = lounge.schema({
+      firstName: String,
+      lastName: String,
+      email: String,
+      dateOfBirth: Date
+    });
+
+    var User = lounge.model('User', userSchema);
+
+    var dob = new Date('March 3, 1989 03:30:00');
+
+    var user = new User({
+      firstName: 'Joe',
+      email: 'joe2@gmail.com'
+    });
+
+    var data = {
+      firstName: 'Joe2',
+      lastName: 'Smith2',
+      dateOfBirth: dob
+    };
+
+    user.save(data, function (err, savedDoc) {
+      expect(err).to.not.be.ok;
+
+      expect(savedDoc).to.be.ok;
+      expect(savedDoc).to.be.an('object');
+      expect(savedDoc.id).to.be.ok;
+      expect(savedDoc.id).to.be.a('string');
+
+      expect(savedDoc.firstName).to.be.equal('Joe2');
+      expect(savedDoc.lastName).to.be.equal('Smith2');
+      expect(savedDoc.email).to.be.equal('joe2@gmail.com');
+      expect(savedDoc.dateOfBirth).to.be.ok;
+      expect(savedDoc.dateOfBirth).to.be.an.instanceof(Date);
+      expect(savedDoc.dateOfBirth.toString()).to.be.equal((new Date(1989, 2, 3, 3, 30, 0)).toString());
+
+      bucket.get(savedDoc.getDocumentKeyValue(true), function (err, dbDoc) {
+        expect(err).to.not.be.ok;
+
+        expect(dbDoc).to.be.ok;
+        expect(dbDoc.value).to.be.ok;
+        expect(dbDoc.value).to.be.an('object');
+
+        var expected = {
+          firstName: 'Joe2',
+          lastName: 'Smith2',
+          email: 'joe2@gmail.com',
           dateOfBirth: dob.toISOString()
         };
 
