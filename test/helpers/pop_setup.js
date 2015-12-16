ba450cbb4ebd1a06d0e01e0d24ffa53551c3263a
Bojan Djurkovic <dbojan@gmail.com>
2015-12-16 17:30:55 -0400

    populate fixes and tests. some minor improvements

--- test/helpers/pop_setup.js
+++ test/helpers/pop_setup.js
@@ -4,38 +4,36 @@ var uuid = require('uuid');
 
 exports.data = {
   users: [{
-    "id": "f1fbfaa8-4ee0-4612-a272-04b092869f4e",
     "firstName": "Bobby",
     "lastName": "Jordan",
     "email": "bjordan0@apple.com",
-    "dateOfBirth": "1958-11-01T23:28:14Z"
+    "dateOfBirth": "1958-11-01T23:28:14Z",
+    "company": "f32a5f17-b827-41b8-83fc-637976a393a5"
   }, {
-    "id": "51bf4ea0-a545-4251-b644-84b849fef9c3",
     "firstName": "Rachel",
     "lastName": "Porter",
     "email": "rporter1@ning.com",
-    "dateOfBirth": "1977-04-23T10:32:14Z"
+    "dateOfBirth": "1977-04-23T10:32:14Z",
+    "company": "company::9be225c9-79a8-4ec4-9113-689a800f825c"
   }, {
-    "id": "5b7205ef-3cbb-442d-89c1-971ebc1bfb85",
     "firstName": "Judith",
     "lastName": "Oliver",
     "email": "joliver2@imgur.com",
-    "dateOfBirth": "1967-10-30T08:36:54Z"
+    "dateOfBirth": "1967-10-30T08:36:54Z",
+    "company": "company::52a1a9b1-5669-4133-9575-2786ebc69635"
   }, {
-    "id": "65374518-54d2-4683-a449-a57e449086fb",
     "firstName": "Kathleen",
     "lastName": "Hernandez",
     "email": "khernandez3@who.int",
     "dateOfBirth": "2014-11-04T07:23:56Z"
   }, {
-    "id": "443047c9-629f-4388-8f16-096d3936a0a2",
     "firstName": "Nicholas",
     "lastName": "Coleman",
     "email": "ncoleman4@europa.eu",
     "dateOfBirth": "1958-04-27T01:21:52Z"
   }],
   companies: [{
-    "id": "company::f32a5f17-b827-41b8-83fc-637976a393a5",
+    "id": "f32a5f17-b827-41b8-83fc-637976a393a5",
     "name": "Jaxspan",
     "streetAddress": "11 Briar Crest Drive",
     "city": "Columbus",
@@ -44,7 +42,7 @@ exports.data = {
     "state": "Ohio",
     "founded": "1999-07-21T03:33:19Z"
   }, {
-    "id": "company::9be225c9-79a8-4ec4-9113-689a800f825c",
+    "id": "9be225c9-79a8-4ec4-9113-689a800f825c",
     "name": "Thoughtstorm",
     "streetAddress": "92862 Autumn Leaf Crossing",
     "city": "Daytona Beach",
@@ -53,7 +51,7 @@ exports.data = {
     "state": "Florida",
     "founded": "1982-01-25T20:00:51Z"
   }, {
-    "id": "company::52a1a9b1-5669-4133-9575-2786ebc69635",
+    "id": "52a1a9b1-5669-4133-9575-2786ebc69635",
     "name": "Blogpad",
     "streetAddress": "3 Vera Pass",
     "city": "Huntsville",
@@ -62,7 +60,7 @@ exports.data = {
     "state": "Texas",
     "founded": "1977-10-13T13:32:24Z"
   }, {
-    "id": "company::343d8f1a-29de-459d-9294-2e44c35f266e",
+    "id": "343d8f1a-29de-459d-9294-2e44c35f266e",
     "name": "Bubbletube",
     "streetAddress": "84942 Mendota Hill",
     "city": "Hampton",
@@ -71,7 +69,7 @@ exports.data = {
     "state": "Virginia",
     "founded": "1995-09-21T15:28:07Z"
   }, {
-    "id": "company::1fabd1fe-72eb-421a-9007-19bfd69f2eb7",
+    "id": "1fabd1fe-72eb-421a-9007-19bfd69f2eb7",
     "name": "Avavee",
     "streetAddress": "334 Knutson Place",
     "city": "Saint Paul",
@@ -79,14 +77,79 @@ exports.data = {
     "postalCode": "55115",
     "state": "Minnesota",
     "founded": "2005-11-15T07:11:04Z"
+  }],
+  posts: [{
+    "id": "f3154355-bfcc-4bf7-b9db-198838399c47",
+    "title": "id mauris vulputate elementum nullam varius nulla",
+    "body": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue.",
+    "comments": ["ddbffba8-f2a8-4f84-b59f-85c778e52463"]
+  }, {
+    "id": "38641ce5-4b5c-45ff-afcf-d4b8b73b5ac0",
+    "title": "interdum mauris non ligula pellentesque ultrices",
+    "body": "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    "comments": ["4fefc12b-df84-410c-aa4a-c8aeb8d85ce0", "36e4b763-baff-4077-9d1d-3d814b3f4ac0"]
+  }, {
+    "id": "8cc6c686-068a-4c26-9a8e-71370ec4e32e",
+    "title": "enim blandit mi in",
+    "body": "Pellentesque eget nunc.",
+    "comments": ["124469a5-e8fd-4546-bc6f-89130b7d74f0", "76232ab0-d5a9-480a-8094-a97d324ba971", "355e3d30-a2df-40f8-98e6-3ceeb8e0fdae"]
+  }, {
+    "id": "4d97535f-3611-4458-954e-f6e4ab566cc1",
+    "title": "quis odio consequat varius integer ac leo",
+    "body": "Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh."
+  }, {
+    "id": "de309406-8529-4643-8ead-0bf260c57a2e",
+    "title": "sapien in sapien iaculis congue vivamus metus",
+    "body": "In blandit ultrices enim.",
+    "comments": []
+  }],
+  comments: [{
+    "id": "ddbffba8-f2a8-4f84-b59f-85c778e52463",
+    "body": "felis donec semper sapien a libero"
+  }, {
+    "id": "4fefc12b-df84-410c-aa4a-c8aeb8d85ce0",
+    "body": "nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel",
+    "user": "ncoleman4@europa.eu"
+  }, {
+    "id": "36e4b763-baff-4077-9d1d-3d814b3f4ac0",
+    "body": "lectus pellentesque eget nunc donec",
+    "user": "rporter1@ning.com"
+  }, {
+    "id": "124469a5-e8fd-4546-bc6f-89130b7d74f0",
+    "body": "sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel",
+    "user": "joliver2@imgur.com"
+  }, {
+    "id": "76232ab0-d5a9-480a-8094-a97d324ba971",
+    "body": "metus aenean fermentum donec ut mauris",
+    "user": "joliver2@imgur.com"
+  }, {
+    "id": "355e3d30-a2df-40f8-98e6-3ceeb8e0fdae",
+    "body": "cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit",
+    "user": "bjordan0@apple.com"
+  }, {
+    "id": "a5cb7e0f-f570-45ec-9229-eb5e5acf43f5",
+    "body": "arcu libero rutrum ac lobortis vel dapibus at diam nam",
+    "user": "ncoleman4@europa.eu"
+  }, {
+    "id": "d33263cd-238c-431f-b250-ffc15b9f20e7",
+    "body": "primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae",
+    "user": "joliver2@imgur.com"
+  }, {
+    "id": "b9ef9373-e3b3-4dc6-952a-39e225503486",
+    "body": "ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis",
+    "user": "ncoleman4@europa.eu"
+  }, {
+    "id": "c3a361cc-4fe6-4316-bdff-38bcf32edcc1",
+    "body": "sit amet sapien dignissim vestibulum vestibulum ante",
+    "user": "bjordan0@apple.com"
   }]
 };
 
 var keymapping = {
-  'users': 'id',
-  'users2': 'email',
-  'users3': 'username',
-  'companies': 'id'
+  'users': 'email',
+  'companies': 'id',
+  'posts': 'id',
+  'comments': 'id'
 };
 
 exports.setup = function (bucket, fn) {
@@ -94,6 +157,10 @@ exports.setup = function (bucket, fn) {
     var objData = exports.data[key];
     async.each(objData, function (obj, eaCb) {
       var docId = obj[keymapping[key]] || obj.id || uuid.v4();
+      if(key === 'companies') {
+        // companies we save with prefix
+        docId = 'company::'.concat(docId);
+      }
       bucket.upsert(docId, obj, eaCb);
     }, easCb);
   }, fn);
--- test/populate.spec.js
+++ test/populate.spec.js
@@ -1,14 +1,312 @@
 var couchbase = require('couchbase');
 var _ = require('lodash');
 var expect = require('chai').expect;
-var ts = require('./helpers/findbyid_setup');
+var ts = require('./helpers/pop_setup');
 
 var lounge = require('../lib');
 var Schema = lounge.Schema;
 
 var bucket;
-var User1, User2, User3, Company;
 
 describe('Model populate tests', function () {
+  before(function (done) {
+    if (lounge) {
+      lounge.disconnect();
+    }
 
+    lounge = new lounge.Lounge(); // recreate it
+
+    var cluster = new couchbase.Mock.Cluster('couchbase://127.0.0.1');
+    bucket = cluster.openBucket('lounge_test', function (err) {
+      lounge.connect({
+        bucket: bucket
+      }, function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        ts.setup(bucket, done);
+      });
+    });
+  });
+
+  describe('with simple ref and boolean populate option', function () {
+    var User, Company, Post, Comment;
+
+    before(function () {
+      var userSchema = lounge.schema({
+        firstName: String,
+        lastName: String,
+        email: {type: String, key: true, generate: false},
+        dateOfBirth: Date,
+        company: {type: String, ref: 'Company'}
+      });
+
+      User = lounge.model('User', userSchema);
+
+      var companySchema = lounge.schema({
+        id: {type: String, key: true, generate: true, prefix: 'company::'},
+        name: String,
+        streetAddress: String,
+        city: String,
+        country: String,
+        state: String,
+        postalCode: String,
+        founded: Date
+      });
+
+      Company = lounge.model('Company', companySchema);
+
+      var commentSchema = lounge.schema({
+        body: String,
+        user: {type: User, ref: 'User'}
+      });
+
+      Comment = lounge.model('Comment', commentSchema);
+
+      var postSchema = lounge.schema({
+        title: String,
+        body: String,
+        comments: [{type: Comment, ref: 'Comment'}]
+      });
+
+      Post = lounge.model('Post', postSchema);
+    });
+
+    it('should get a document and not populate anything with no option', function (done) {
+      var userId = ts.data.users[0].email;
+      var userData = ts.data.users[0];
+
+      User.findById(userId, function (err, rdoc) {
+        expect(err).to.not.be.ok;
+
+        expect(rdoc).to.be.ok;
+        expect(rdoc).to.be.an('object');
+        expect(rdoc).to.be.an.instanceof(User);
+        expect(rdoc.id).to.not.be.ok;
+
+        expect(rdoc.firstName).to.equal(userData.firstName);
+        expect(rdoc.lastName).to.equal(userData.lastName);
+        expect(rdoc.email).to.equal(userData.email);
+        expect(rdoc.company).to.equal(userData.company);
+        expect(rdoc.dateOfBirth).to.be.ok;
+        expect(rdoc.dateOfBirth).to.be.an.instanceof(Date);
+
+        var cas3 = rdoc.cas;
+        expect(cas3).to.be.a('string');
+
+        done();
+      });
+    });
+
+    it('should get a document and populate refs with populate option = true', function (done) {
+      var userId = ts.data.users[0].email;
+      var userData = ts.data.users[0];
+
+      User.findById(userId, {populate: true}, function (err, rdoc, missed) {
+        expect(err).to.not.be.ok;
+
+        expect(rdoc).to.be.ok;
+        expect(rdoc).to.be.an('object');
+        expect(rdoc).to.be.an.instanceof(User);
+        expect(rdoc.id).to.not.be.ok;
+
+        expect(rdoc.firstName).to.equal(userData.firstName);
+        expect(rdoc.lastName).to.equal(userData.lastName);
+        expect(rdoc.email).to.equal(userData.email);
+        expect(rdoc.dateOfBirth).to.be.ok;
+        expect(rdoc.dateOfBirth).to.be.an.instanceof(Date);
+
+        var cas = rdoc.cas;
+        expect(cas).to.be.a('string');
+
+        expect(rdoc.company).to.be.ok;
+        expect(rdoc.company).to.be.an('object');
+        expect(rdoc.company).to.be.an.instanceof(Company);
+
+        var companyData = ts.data.companies[0];
+
+        expect(rdoc.company.id).to.equal(companyData.id);
+        expect(rdoc.company.name).to.equal(companyData.name);
+        expect(rdoc.company.streetAddress).to.equal(companyData.streetAddress);
+        expect(rdoc.company.city).to.equal(companyData.city);
+        expect(rdoc.company.country).to.equal(companyData.country);
+        expect(rdoc.company.state).to.equal(companyData.state);
+        expect(rdoc.company.postalCode).to.equal(companyData.postalCode);
+        expect(rdoc.company.founded).to.be.ok;
+        expect(rdoc.company.founded).to.be.an.instanceof(Date);
+
+        var cas2 = rdoc.company.cas;
+        expect(cas2).to.be.a('string');
+
+        expect(cas).to.not.equal(cas2);
+
+        expect(missed).to.be.an.instanceof(Array);
+        expect(missed.length).to.equal(0);
+
+        done();
+      });
+    });
+
+    it('should get a document and populate refs with full reference id and populate option = true', function (done) {
+      var userId = ts.data.users[1].email;
+      var userData = ts.data.users[1];
+
+      User.findById(userId, {populate: true}, function (err, rdoc, missed) {
+        expect(err).to.not.be.ok;
+
+        expect(rdoc).to.be.ok;
+        expect(rdoc).to.be.an('object');
+        expect(rdoc).to.be.an.instanceof(User);
+        expect(rdoc.id).to.not.be.ok;
+
+        expect(rdoc.firstName).to.equal(userData.firstName);
+        expect(rdoc.lastName).to.equal(userData.lastName);
+        expect(rdoc.email).to.equal(userData.email);
+        expect(rdoc.dateOfBirth).to.be.ok;
+        expect(rdoc.dateOfBirth).to.be.an.instanceof(Date);
+
+        var cas = rdoc.cas;
+        expect(cas).to.be.a('string');
+
+        expect(rdoc.company).to.be.ok;
+        expect(rdoc.company).to.be.an('object');
+        expect(rdoc.company).to.be.an.instanceof(Company);
+
+        var companyData = ts.data.companies[1];
+
+        expect(rdoc.company.id).to.equal(companyData.id);
+        expect(rdoc.company.name).to.equal(companyData.name);
+        expect(rdoc.company.streetAddress).to.equal(companyData.streetAddress);
+        expect(rdoc.company.city).to.equal(companyData.city);
+        expect(rdoc.company.country).to.equal(companyData.country);
+        expect(rdoc.company.state).to.equal(companyData.state);
+        expect(rdoc.company.postalCode).to.equal(companyData.postalCode);
+        expect(rdoc.company.founded).to.be.ok;
+        expect(rdoc.company.founded).to.be.an.instanceof(Date);
+
+        var cas2 = rdoc.company.cas;
+        expect(cas2).to.be.a('string');
+
+        expect(cas).to.not.equal(cas2);
+
+        expect(missed).to.be.an.instanceof(Array);
+        expect(missed.length).to.equal(0);
+
+        done();
+      });
+    });
+
+    it('should work with arrays in findById', function (done) {
+      var userIds = _.pluck(ts.data.users, 'email');
+
+      User.findById(userIds, {populate: true}, function (err, rdocs, missed) {
+        expect(err).to.not.be.ok;
+
+        expect(rdocs).to.be.ok;
+        expect(rdocs).to.be.an.instanceof(Array);
+        expect(rdocs.length).to.equal(ts.data.users.length);
+
+        expect(missed).to.be.an.instanceof(Array);
+        expect(missed.length).to.equal(0);
+
+        rdocs = _.sortBy(rdocs, 'email');
+
+        var expectedUsers = _.cloneDeep(_.sortBy(ts.data.users, 'email'));
+
+        rdocs.forEach(function (rdoc, index) {
+
+          var userData = expectedUsers[index];
+
+          var companyData;
+
+          if (userData.email === 'bjordan0@apple.com') {
+            companyData = ts.data.companies[0];
+          }
+          else if (userData.email === 'rporter1@ning.com') {
+            companyData = ts.data.companies[1];
+          }
+          else if (userData.email === 'joliver2@imgur.com') {
+            companyData = ts.data.companies[2];
+          }
+
+          expect(rdoc).to.be.ok;
+          expect(rdoc).to.be.an('object');
+          expect(rdoc).to.be.an.instanceof(User);
+          expect(rdoc.id).to.not.be.ok;
+
+          expect(rdoc.firstName).to.equal(userData.firstName);
+          expect(rdoc.lastName).to.equal(userData.lastName);
+          expect(rdoc.email).to.equal(userData.email);
+          expect(rdoc.dateOfBirth).to.be.ok;
+          expect(rdoc.dateOfBirth).to.be.an.instanceof(Date);
+
+          var cas = rdoc.cas;
+          expect(cas).to.be.a('string');
+
+          if (companyData) {
+            expect(rdoc.company).to.be.ok;
+            expect(rdoc.company).to.be.an('object');
+            expect(rdoc.company).to.be.an.instanceof(Company);
+
+            expect(rdoc.company.id).to.equal(companyData.id);
+            expect(rdoc.company.name).to.equal(companyData.name);
+            expect(rdoc.company.streetAddress).to.equal(companyData.streetAddress);
+            expect(rdoc.company.city).to.equal(companyData.city);
+            expect(rdoc.company.country).to.equal(companyData.country);
+            expect(rdoc.company.state).to.equal(companyData.state);
+            expect(rdoc.company.postalCode).to.equal(companyData.postalCode);
+            expect(rdoc.company.founded).to.be.ok;
+            expect(rdoc.company.founded).to.be.an.instanceof(Date);
+
+            var cas2 = rdoc.company.cas;
+            expect(cas2).to.be.a('string');
+
+            expect(cas).to.not.equal(cas2);
+          }
+          else {
+            expect(rdoc.company).to.not.be.ok;
+          }
+        });
+
+        done();
+      });
+    });
+
+    it('should work with array refs', function (done) {
+      var postId = ts.data.posts[0].id;
+      var expectedData = ts.data.posts[0];
+
+      Post.findById(postId, {populate: true}, function (err, rdoc, missed) {
+        //console.log(rdoc);
+        expect(err).to.not.be.ok;
+
+        expect(rdoc).to.be.ok;
+        expect(rdoc).to.be.an('object');
+        expect(rdoc).to.be.an.instanceof(Post);
+
+        expect(missed).to.be.an.instanceof(Array);
+        expect(missed.length).to.equal(0);
+
+        expect(rdoc.id).to.equal(expectedData.id);
+        expect(rdoc.title).to.equal(expectedData.title);
+        expect(rdoc.body).to.equal(expectedData.body);
+
+        var expectedComments = _.sortBy([ts.data.comments[0]], 'id');
+        expect(rdoc.comments).to.be.an.instanceof(Array);
+
+        var actualComments = _.sortBy(rdoc.comments, 'id');
+
+        actualComments.forEach(function(ac, i) {
+          var expectedComment = expectedComments[i];
+
+          expect(ac.id).to.equal(expectedComment.id);
+          expect(ac.title).to.equal(expectedComment.title);
+          expect(ac.body).to.equal(expectedComment.body);
+        });
+
+        done();
+      });
+    });
+  });
 });
