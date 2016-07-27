6f7b45df9a46a0163ee1de04334dd08a190a762c
Bojan Djurkovic <dbojan@gmail.com>
2016-07-27 16:26:07 -0300

    updated documentation

--- test/helpers/utils.js
+++
@@ -1,7 +0,0 @@
-var couchbase = require('couchbase');
-
-exports.getCluser = function () {
-  return process.env.LOUNGE_COUCHBASE_MOCK ?
-    new couchbase.Mock.Cluster('couchbase://127.0.0.1') :
-    new couchbase.Cluster('couchbase://127.0.0.1');
-};
