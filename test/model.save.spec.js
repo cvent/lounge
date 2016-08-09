4252abbbb752a672eab37817df5ae6b6a9b2998c
Bojan Djurkovic <dbojan@gmail.com>
2016-08-08 22:59:55 -0300

    fix promisifyCall

--- test/model.save.spec.js
+++ test/model.save.spec.js
@@ -87,6 +87,62 @@ describe('Model save tests', function () {
     });
   });
 
+  it('should save a simple document - promised', function (done) {
+    var userSchema = lounge.schema({
+      firstName: String,
+      lastName: String,
+      email: String,
+      dateOfBirth: Date
+    });
+
+    var User = lounge.model('User', userSchema);
+
+    var dob = new Date('March 3, 1990 03:30:00');
+
+    var user = new User({
+      firstName: 'Joe',
+      lastName: 'Smith',
+      email: 'joe@gmail.com',
+      dateOfBirth: dob
+    });
+
+    var p = user.save();
+    p.then(function (savedDoc) {
+      expect(savedDoc).to.be.an('object');
+      expect(savedDoc.id).to.be.ok;
+      expect(savedDoc.id).to.be.a('string');
+
+      expect(savedDoc.firstName).to.equal('Joe');
+      expect(savedDoc.lastName).to.equal('Smith');
+      expect(savedDoc.email).to.equal('joe@gmail.com');
+      expect(savedDoc.dateOfBirth).to.be.ok;
+      expect(savedDoc.dateOfBirth).to.be.an.instanceof(Date);
+      expect(savedDoc.dateOfBirth.toString()).to.equal((new Date(1990, 2, 3, 3, 30, 0)).toString());
+
+      expect(savedDoc.cas).to.be.ok;
+
+      bucket.get(savedDoc.getDocumentKeyValue(true), function (err, dbDoc) {
+        expect(err).to.not.be.ok;
+
+        expect(dbDoc).to.be.ok;
+        expect(dbDoc.value).to.be.ok;
+        expect(dbDoc.value).to.be.an('object');
+
+        var expected = {
+          firstName: 'Joe',
+          lastName: 'Smith',
+          email: 'joe@gmail.com',
+          dateOfBirth: dob.toISOString()
+        };
+
+        expected.id = savedDoc.getDocumentKeyValue(true);
+
+        expect(dbDoc.value).to.deep.equal(expected);
+        done();
+      });
+    });
+  });
+
   it('should save a simple document with invisible left out', function (done) {
     var userSchema = lounge.schema({
       firstName: String,
