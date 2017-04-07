314d8f067576ad84928ef5e9085f1b17ad621d5b
Bojan Djurkovic <dbojan@gmail.com>
2017-04-07 18:29:39 -0400

    cleanup

--- mvapidocs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-require('shelljs/global');
-var v = require('./package.json').version;
-srcdir = __dirname + '/api-docs';
-var src = __dirname + '/api-docs/lounge/' + v + '/*';
-var dest = __dirname + '/docs/api-docs';
-console.log(src)
-console.log(dest)
-mv(src, dest);
